898981d22ce4bc02d86a134a7520c2f63ab7bd3d
Bikash Mohapatra <tech.mbikash@gmail.com>
2024-11-08 01:39:52 +0530

    Day 3 : Code Added.

--- src/com/eazybytes/java7/BinaryLiteralsJava7Feature.java
+++ src/com/eazybytes/java7/BinaryLiteralsJava7Feature.java
@@ -1,17 +1,21 @@
 package com.eazybytes.java7;
 
+import java.util.logging.Logger;
+
 public class BinaryLiteralsJava7Feature {
+    public static final Logger LOGGER = Logger.getLogger(BinaryLiteralsJava7Feature.class.getName());
     public static void main(String[] args) {
+
 
         byte byteBinary = 0b111;
         short shortBinary = 0b001;
         int intBinary = 0b101;
         long longBinary = 0b0001010111;
 
-        System.out.println("The value of Byte number is : "+byteBinary);
-        System.out.println("The value of Short number is : "+shortBinary);
-        System.out.println("The value of Int number is : "+intBinary);
-        System.out.println("The value of Long number is : "+longBinary);
+        LOGGER.info("The value of Byte number is : "+byteBinary);
+        LOGGER.info("The value of Short number is : "+shortBinary);
+        LOGGER.info("The value of Int number is : "+intBinary);
+        LOGGER.info("The value of Long number is : "+longBinary);
 
     }
 }
