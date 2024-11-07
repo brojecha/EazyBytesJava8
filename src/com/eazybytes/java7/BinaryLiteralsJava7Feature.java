d3d82b3547c0fb9695d787ee735a09f9f290aa18
Bikash Mohapatra <tech.mbikash@gmail.com>
2024-11-08 01:34:53 +0530

    Day 3 : Code Added.

---
+++ src/com/eazybytes/java7/BinaryLiteralsJava7Feature.java
@@ -0,0 +1,17 @@
+package com.eazybytes.java7;
+
+public class BinaryLiteralsJava7Feature {
+    public static void main(String[] args) {
+
+        byte byteBinary = 0b111;
+        short shortBinary = 0b001;
+        int intBinary = 0b101;
+        long longBinary = 0b0001010111;
+
+        System.out.println("The value of Byte number is : "+byteBinary);
+        System.out.println("The value of Short number is : "+shortBinary);
+        System.out.println("The value of Int number is : "+intBinary);
+        System.out.println("The value of Long number is : "+longBinary);
+
+    }
+}
